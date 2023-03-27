0cbceffafbea025f06e91f44028e61e3118c4b85
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-27 12:29:04 +0200

    Számok reciprok értéke

---
+++ 035. reciprok/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _035.reciprok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            Console.WriteLine("Kérek egy számot és kiírom a reciprokát!");
+            Console.Write("a=");
+            a = double.Parse(Console.ReadLine());
+            double c = 1 / a;
+
+            Console.WriteLine("A számod reciproka: " + c);
+
+            Console.ReadKey();
+        }
+    }
+}
